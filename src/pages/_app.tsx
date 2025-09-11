c51726d8fa04bdfd1e580f2f2f8ea0b283cc1928
Sarvesh Max <sarmaxsri@yahoo.com>
2025-09-11 23:30:19 +0530

    adding googletagmanager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { FC } from "react";
 import { ContextProvider } from "../contexts/ContextProvider";
 import { AppBar } from "../components/AppBar";
@@ -10,12 +11,28 @@ import "/styles.css";
 require("@solana/wallet-adapter-react-ui/styles.css");
 require("../styles/globals.css");
 
+const GA_TRACKING_ID = "AW-17550958377";
+
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>KeyGlowSolana</title>
+        <title>SolanaTokenBurner</title>
       </Head>
+
+      {/* Google Tag Scripts */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
 
       <ContextProvider>
         <div className="flex flex-col h-screen">
